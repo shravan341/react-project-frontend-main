dcfadc1319649ccd1a024d0f1546d349890884d9
Shravan Kumar <40226836+shravan341@users.noreply.github.com>
2024-07-29 19:56:31 +0530

    first commit

---
+++ react-project-frontend-main/src/hooks/useCreateArticle.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const creteArticle = async (values) => {
+  console.log("creteArticle", { values });
+
+  const { data } = await axios.post(
+    `https://react-project-backend-1-ivv0.onrender.com/api/articles`,
+    {
+      article: { ...values.values },
+    }
+  );
+
+  console.log("creteArticleresult", { data });
+
+  return data;
+};
+
+export default function useCreateArticle() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const { mutate: createArticle, isLoading: isCreating } = useMutation({
+    mutationFn: creteArticle,
+    onSuccess: () => {
+      alert("New post successfully created");
+      queryClient.invalidateQueries({ queryKey: ["articles"] });
+      navigate("/");
+    },
+    onError: (err) => alert(err.message),
+  });
+
+  return { isCreating, createArticle };
+}
